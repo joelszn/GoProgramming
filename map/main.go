65d8adc3222563cb5684559136659d9dadea1329
joelszn <duranjoel.a@gmail.com>
2019-01-22 13:27:21 -0500

    back on git, maps in Go

---
+++ map/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declaring a map that all the keys are of type string & same for values
+	colors := map[string]string{
+		"red":   "#FF0000",
+		"green": "#00FF00",
+	}
+	fmt.Println(colors)
+}

daaf6e8f737f8728ae9b81097dd75be491ace648
joelszn <duranjoel.a@gmail.com>
2019-01-24 18:26:00 -0500

    cool HTTP GET that returns HTML of specified site

--- interfaces_http/main.go
+++ interfaces_http/main.go
@@ -7,11 +7,14 @@ import (
 )
 
 func main() {
-	resp, err := http.Get("http://google.com")
+	resp, err := http.Get("http://joelszn.github.io")
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	// make a byte slice with n amount of empty space
+	bs := make([]byte, 99999)
+	resp.Body.Read(bs)
+	fmt.Println(string(bs))
 }
